603c0840273ebd896c11e64bba66d2304c6a529c
Renad_Alalfy <renadalalfy@gmail.com>
2025-09-10 23:40:09 +0300

    Refactor: remove unused SharedPreferences import

--- app/src/main/java/com/karrar/movieapp/data/local/AppConfiguration.kt
+++ app/src/main/java/com/karrar/movieapp/data/local/AppConfiguration.kt
@@ -1,7 +1,6 @@
 package com.karrar.movieapp.data.local
 
 
-import android.content.SharedPreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject

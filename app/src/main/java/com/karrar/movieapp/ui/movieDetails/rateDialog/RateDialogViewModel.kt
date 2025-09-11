d51c491095cdd871c7605bba0e153243c5bf1637
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-09-12 00:29:08 +0300

    feat: support series rate

--- app/src/main/java/com/karrar/movieapp/ui/movieDetails/rateDialog/RateDialogViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/movieDetails/rateDialog/RateDialogViewModel.kt
@@ -2,12 +2,14 @@ package com.karrar.movieapp.ui.movieDetails.rateDialog
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
-import com.karrar.movieapp.domain.usecases.GetSessionIDUseCase
+import com.karrar.movieapp.domain.usecases.movieDetails.DeleteMovieRatingUseCase
 import com.karrar.movieapp.domain.usecases.movieDetails.GetMovieRateUseCase
-import com.karrar.movieapp.domain.usecases.movieDetails.SetRatingUseCase
+import com.karrar.movieapp.domain.usecases.tvShowDetails.DeleteSeriesRatingUseCase
+import com.karrar.movieapp.domain.usecases.tvShowDetails.GetTvShowDetailsUseCase
+import com.karrar.movieapp.domain.usecases.movieDetails.SetRatingUseCase as SetMovieRatingUseCase
+import com.karrar.movieapp.domain.usecases.tvShowDetails.SetRatingUesCase as SetTvShowsRatingUseCase
 import com.karrar.movieapp.ui.base.BaseViewModel
-import com.karrar.movieapp.ui.movieDetails.MovieDetailsUIEvent
-import com.karrar.movieapp.ui.movieDetails.saveMovie.SaveMovieDialogArgs
+import com.karrar.movieapp.utilities.Constants
 import com.karrar.movieapp.utilities.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,7 +21,11 @@ import javax.inject.Inject
 @HiltViewModel
 class RateDialogViewModel @Inject constructor(
     private val getMovieRateUseCase: GetMovieRateUseCase,
-    private val setRatingUseCase: SetRatingUseCase,
+    private val getTvShowsRatingUseCase: GetTvShowDetailsUseCase,
+    private val setMovieRatingUseCase: SetMovieRatingUseCase,
+    private val setTvShowsRatingUseCase: SetTvShowsRatingUseCase,
+    private val deleteMovieRatingUseCase: DeleteMovieRatingUseCase,
+    private val deleteSeriesRatingUseCase: DeleteSeriesRatingUseCase,
     state: SavedStateHandle,
 ) : BaseViewModel(), RateDialogInteractionListener {
 
@@ -41,7 +47,20 @@ class RateDialogViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 val mediaId = args.mediaId
-                val rate = getMovieRateUseCase(mediaId)
+                val mediaType = args.mediaType
+                val rate = when (mediaType) {
+                    Constants.MOVIE -> {
+                        getMovieRateUseCase(mediaId)
+                    }
+
+                    Constants.TV_SHOWS -> {
+                        getTvShowsRatingUseCase.getTvShowRated(mediaId)
+                    }
+
+                    else -> {
+                        0.0f
+                    }
+                }
                 _rateDialogUIState.update { it.copy(rate = rate) }
             } catch (t: Throwable) {
             }
@@ -54,6 +73,7 @@ class RateDialogViewModel @Inject constructor(
 
     override fun onSubmitClick() {
         val mediaId = args.mediaId
+        val mediaType = args.mediaType
         val rate = _rateDialogUIState.value.rate
         if (rate == null) {
             _rateDialogUIEvent.update { Event(RateDialogUIEvent.ShowMessage("Please select a rating")) }
@@ -61,8 +81,39 @@ class RateDialogViewModel @Inject constructor(
         }
         viewModelScope.launch {
             try {
-                setRatingUseCase(mediaId, rate)
+                when (mediaType) {
+                    Constants.MOVIE -> {
+                        setMovieRatingUseCase(mediaId, rate)
+                    }
+
+                    Constants.TV_SHOWS -> {
+                        setTvShowsRatingUseCase(mediaId, rate)
+                    }
+                }
                 _rateDialogUIEvent.update { Event(RateDialogUIEvent.ShowMessage("We submitted your rate")) }
+                onCancelClick()
+            } catch (e: Throwable) {
+                _rateDialogUIEvent.update { Event(RateDialogUIEvent.ShowMessage("Error occurred. Please try again.")) }
+            }
+        }
+    }
+
+    override fun onDeleteClick() {
+        val mediaId = args.mediaId
+        val mediaType = args.mediaType
+        viewModelScope.launch {
+            try {
+                when (mediaType) {
+                    Constants.MOVIE -> {
+                        deleteMovieRatingUseCase(mediaId)
+                    }
+
+                    Constants.TV_SHOWS -> {
+                        deleteSeriesRatingUseCase(mediaId)
+                    }
+                }
+                _rateDialogUIEvent.update { Event(RateDialogUIEvent.ShowMessage("We removed your rate")) }
+                onCancelClick()
             } catch (e: Throwable) {
                 _rateDialogUIEvent.update { Event(RateDialogUIEvent.ShowMessage("Error occurred. Please try again.")) }
             }

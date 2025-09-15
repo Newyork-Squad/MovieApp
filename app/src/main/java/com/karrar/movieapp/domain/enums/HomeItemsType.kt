88521cb537e85d54a6bacaf5b3ce1cb8b40bcb77
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-09-16 01:35:08 +0300

    refactor: update strings

--- app/src/main/java/com/karrar/movieapp/domain/enums/HomeItemsType.kt
+++ app/src/main/java/com/karrar/movieapp/domain/enums/HomeItemsType.kt
@@ -1,8 +1,8 @@
 package com.karrar.movieapp.domain.enums
 
 enum class HomeItemsType(val value :String) {
-    TOP_RATED_TV_SHOWS("Top rated TV shows"),
-    RECENTLY_RELEASED("Recently released"),
+    TOP_RATED_TV_SHOWS("Top Rated TV Shows"),
+    RECENTLY_RELEASED("Recently Released"),
     UPCOMING("Upcoming"),
     RECENTLY_VIEWED("Recently viewed"),
     COLLECTIONS("Your Collections"),

d835ba79490ac132e6be127c792f92efae420308
Abdallah Bakhet <abakhet2010@gmail.com>
2025-09-24 23:12:22 +0300

    refactor: update MatchQuestionsFragment to use MatchViewModel and MatchUiState

--- app/src/main/java/com/karrar/movieapp/ui/match/questions/MatchQuestionsFragment.kt
+++ app/src/main/java/com/karrar/movieapp/ui/match/questions/MatchQuestionsFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.karrar.movieapp.R
 import com.karrar.movieapp.databinding.FragmentMatchQuestionsBinding
+import com.karrar.movieapp.domain.enums.MediaType
 import com.karrar.movieapp.ui.base.BaseFragment
+import com.karrar.movieapp.ui.match.MatchUiState
+import com.karrar.movieapp.ui.match.MatchViewModel
 import com.karrar.movieapp.ui.match.questions.adapter.MatchAdapter
+import com.karrar.movieapp.ui.match.result.MatchResultFragmentDirections
 import com.karrar.movieapp.ui.match.result.MatchUiEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -19,7 +22,7 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MatchQuestionsFragment : BaseFragment<FragmentMatchQuestionsBinding>() {
     override val layoutIdFragment: Int = R.layout.fragment_match_questions
-    override val viewModel: MatchQuestionsViewModel by activityViewModels()
+    override val viewModel: MatchViewModel by activityViewModels()
     var currentMathType = MatchQuestionType.MOOD
     private lateinit var questionAdapter: MatchAdapter
 
@@ -60,28 +63,60 @@ class MatchQuestionsFragment : BaseFragment<FragmentMatchQuestionsBinding>() {
 
                 when (event) {
                     MatchUiEvent.NavigateToResults -> {
-                        val action = MatchQuestionsFragmentDirections
-                            .actionMatchQuestionsFragmentToMatchResultFragment()
+                        val action =
+                            MatchQuestionsFragmentDirections.actionMatchQuestionsFragmentToMatchResultFragment()
                         findNavController().navigate(action)
                     }
 
                     MatchUiEvent.ShowNoMoviesToast -> {
-                        Toast.makeText(
-                            requireContext(),
-                            "No matching movies found",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        findNavController().popBackStack()
+                        Toast
+                            .makeText(
+                                requireContext(),
+                                "No matching movies found",
+                                Toast.LENGTH_SHORT,
+                            ).show()
                     }
-                    MatchUiEvent.NavigateBack -> TODO()
-                    is MatchUiEvent.PlayYoutubeTrailer -> TODO()
-                    is MatchUiEvent.SaveMovie -> TODO()
-                    MatchUiEvent.ViewMovieDetails -> TODO()
+                    MatchUiEvent.NavigateBack -> {
+                        findNavController().popBackStack(R.id.homeFragment, false)
+                    }
+                    is MatchUiEvent.PlayYoutubeTrailer -> {
+                        val action =
+                            MatchResultFragmentDirections
+                                .actionMatchResultFragmentToYoutubePlayerActivity(
+                                    event.movieId,
+                                    MediaType.MOVIE,
+                                )
+
+                        findNavController().navigate(action)
+                    }
+
+                    is MatchUiEvent.SaveMovie -> {
+                        val action =
+                            MatchResultFragmentDirections
+                                .actionMatchResultFragmentToSaveMovieDialog(event.movieId)
+                        findNavController().navigate(action)
+                    }
+
+                    is MatchUiEvent.ViewMovieDetails -> {
+                        val action =
+                            MatchResultFragmentDirections
+                                .actionMatchResultFragmentToMovieDetailFragment(event.movieId)
+                        findNavController().navigate(action)
+                    }
+
+                    MatchUiEvent.ShowLoginDialogEvent -> {
+                        val action =
+                            MatchResultFragmentDirections
+                                .actionMatchResultFragmentToLoginDialog()
+                        findNavController().navigate(action)
+                    }
                 }
             }
         }
     }
 
-    private fun render(state: MatchQuestionUiState) {
+    private fun render(state: MatchUiState) {
         with(binding) {
             currentMathType = state.currentQuestionType
             progressIndicator.setProgressCompat(state.progress, true)
